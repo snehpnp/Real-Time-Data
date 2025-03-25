b4827699162a7a83de5dc9937e201ccefae828b6
snehpnp <snehpnp@gmail.com>
2025-03-25 11:27:24 +0530

    Auto-update: Extend Stock model with additional fields for trading data

--- models/Stock.js
+++ models/Stock.js
@@ -6,6 +6,15 @@ const StockSchema = new mongoose.Schema({
   exc: String, // Exchange
   curTime: Number, // Timestamp
   ft: String, // Future Trade Indicator
+  t: String, // Trade Indicator
+  pc: String, // Previous Close
+  v: String, // Volume
+  bp1: String, // Best Buy Price
+  sp1: String, // Best Sell Price
+  bq1: String, // Best Buy Quantity
+  sq1: String, // Best Sell Quantity
+  token: String,
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Stock", StockSchema);
